4d0d5d652b0d7e8b8c3cd9dd9904f446562fc55e
saole <lethisao1894@gmail.com>
2017-02-23 11:38:42 +0700

    asdasd

--- resources/views/content/company_images.blade.php
+++ resources/views/content/company_images.blade.php
@@ -15,16 +15,14 @@
       <div class="content_top_right">
         <ul class="featured_nav wow fadeInDown">
           @foreach($company_images as $image)
-          <div class="row">
-             <li class=" col-xs-3">
-             <div class="imgWrap text-center item-video">
-              <div class="thumb-smaller" style="background-image:url({!! asset('uploads/'.$image->image) !!});"></div>
-              <p class="imgDescription" style="padding-top: 70px;">{{$image->description}}</p>
-              <div class="title_caption">{{$image->name}}</div>
-            </div>
-          </li>
-          </div>
-         
+            <li>
+               <div class="imgWrap text-center item-video">
+                <div class="thumb-smaller" style="background-image:url({!! asset('uploads/'.$image->image) !!});"></div>
+                <p class="imgDescription" style="padding-top: 70px;">{{$image->description}}</p>
+                <div class="title_caption">{{$image->name}}</div>
+              </div>
+            </li>
+
           @endforeach
         </ul>
       </div>
