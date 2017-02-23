0eaa4ace2d3b4bba3b1d727760e285ec4be51248
saole <lethisao1894@gmail.com>
2017-02-23 19:06:24 +0700

    adasd

--- resources/views/content/company_images.blade.php
+++ resources/views/content/company_images.blade.php
@@ -11,7 +11,7 @@
         </div>
       </div>
     </div>
-    <div class="col-lg-6 col-md-6 col-sm-6" id="image-right" style="color: #fff">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" id="image-right" style="color: #fff">
       <div class="content_top_right">
         <ul class="featured_nav wow fadeInDown">
           @foreach($company_images as $image)
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,13 +8,13 @@
               
           <!--start: Logo -->
 
-          <div class="logo col-lg-3 col-md-3 col-sm-3">
+          <div class="logo col-lg-3 col-md-3 col-sm-3 col-xs-6 text-center">
                   
-              <a class="brand" href="#"><img src="{!! asset('css/img/logo-group.png') !!}" alt="Logo" class="logo"></a>
+              <img src="{!! asset('css/img/logo-group.png') !!}" alt="Logo" class="img-responsive">
                   
           </div >
-          <div class="ol-lg-2 col-md-2 col-sm-2"><img class="letgo" alt="" src="{!! asset('css/img/letgo.png') !!}" ></div>
-          <div class="app-logo col-lg-6 col-md-6 col-sm-6" >
+          <div class="ol-lg-2 col-md-2 col-sm-2 col-xs-12 text-center"><img class="letgo img-responsive" alt="" src="{!! asset('css/img/letgo.png') !!}" ></div>
+          <div class="app-logo col-lg-6 col-md-6 col-sm-6 col-xs-10" >
             @if($apps != null)
                 @foreach($apps as $app)
                   <img class="img" alt="" src="{!! asset('uploads/'.$app->logo) !!}" >
@@ -23,7 +23,7 @@
 
           </div>
           <!--start: Logo -->
-          <div class="col-lg-1 col-md-1 col-ms-2 image-lang" >
+          <div class="col-lg-1 col-md-1 col-ms-2 col-xs-2 image-lang" >
                   
               <?php 
             $name   = '';
