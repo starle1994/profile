662d948f6433241717847f90a7b99cfba3ea8375
le star <le star>
2017-03-07 11:04:02 -0800

    aaaa

--- resources/views/admin/companyimages/create.blade.php
+++ resources/views/admin/companyimages/create.blade.php
@@ -39,13 +39,6 @@
         
     </div>
 </div>
-
-<div class="form-group">
-    <div class="col-sm-10 col-sm-offset-2">
-      {!! Form::submit( trans('quickadmin::templates.templates-view_create-create') , array('class' => 'btn btn-primary')) !!}
-    </div>
-</div>
-
 {!! Form::close() !!}
 <script type="text/javascript">
         Dropzone.options.imageUpload = {
