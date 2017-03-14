8897abaa18bc47d7eed05222da60b8927868e40d
saole <lethisao1894@gmail.com>
2017-03-14 11:57:12 +0700

    dsds

--- app/MySchedule.php
+++ app/MySchedule.php
@@ -43,57 +43,7 @@ class MySchedule extends Model {
      * Set attribute to datetime format
      * @param $input
      */
-    public function setStartTimeAttribute($input)
-    {
-        if($input != '') {
-            $this->attributes['start_time'] = Carbon::createFromFormat(config('quickadmin.date_format') . ' ' . config('quickadmin.time_format'), $input)->format('Y-m-d H:i:s');
-        }else{
-            $this->attributes['start_time'] = '';
-        }
-    }
-
-    /**
-     * Get attribute from datetime format
-     * @param $input
-     *
-     * @return string
-     */
-    public function getStartTimeAttribute($input)
-    {
-        if($input != '0000-00-00') {
-            return Carbon::createFromFormat('Y-m-d H:i:s', $input)->format(config('quickadmin.date_format') . ' ' .config('quickadmin.time_format'));
-        }else{
-            return '';
-        }
-    }
-
-/**
-     * Set attribute to datetime format
-     * @param $input
-     */
-    public function setEndTimeAttribute($input)
-    {
-        if($input != '') {
-            $this->attributes['end_time'] = Carbon::createFromFormat(config('quickadmin.date_format') . ' ' . config('quickadmin.time_format'), $input)->format('Y-m-d H:i:s');
-        }else{
-            $this->attributes['end_time'] = '';
-        }
-    }
-
-    /**
-     * Get attribute from datetime format
-     * @param $input
-     *
-     * @return string
-     */
-    public function getEndTimeAttribute($input)
-    {
-        if($input != '0000-00-00') {
-            return Carbon::createFromFormat('Y-m-d H:i:s', $input)->format(config('quickadmin.date_format') . ' ' .config('quickadmin.time_format'));
-        }else{
-            return '';
-        }
-    }
+    
 
 
 }
